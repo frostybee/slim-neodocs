41d9cf309597d79786c339fbf608d8a994f571ed
frostybee <rsleiman2@gmail.com>
2025-06-25 19:43:05 -0400

    }

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -107,7 +107,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'docSidebar',
             position: 'left',
             label: 'Documentation',
           },
@@ -137,7 +137,7 @@ const config = {
             items: [
               {
                 label: 'Documentation',
-                to: '/docs/intro',
+                to: '/docs/v4',
               },
             ],
           },
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/v4">
+            Read the Documentation 📚
           </Link>
         </div>
       </div>
